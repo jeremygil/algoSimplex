09ef3b39660e69bfcf6e4812604c6898c6529f85
VGreg62 <gregory.viandier@outlook.fr>
2020-03-10 14:11:33 +0100

    Add method

--- algoSimplex/algoSimplex/Program.cs
+++ algoSimplex/algoSimplex/Program.cs
@@ -17,6 +17,7 @@ namespace algoSimplex
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
