258943da59230b6738452b7b33eb00b56d00224e
VGreg62 <gregory.viandier@outlook.fr>
2020-03-17 15:49:27 +0100

    Add min, max, cj-zj

--- algoSimplex/UnitTest_Simplex/UnitTestBack.cs
+++ algoSimplex/UnitTest_Simplex/UnitTestBack.cs
@@ -1,12 +1,15 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using algoSimplex;
 using System.Collections.Generic;
+using log4net;
 
 namespace UnitTest_Simplex
 {
     [TestClass]
     public class UnitTestBack
     {
+        private static ILog LOGGER = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         [TestMethod]
         public void TestCalculZ()
         {
@@ -20,8 +23,11 @@ namespace UnitTest_Simplex
             //Resultat
             double testValue = 0.0;
 
+            //Affichage valeur
+            LOGGER.Info("Valeur de Z : " + value);
+
             //Test
-            Assert.IsTrue(value == testValue, "C'est vrai");
+            Assert.AreEqual(value, testValue);
         }
 
         [TestMethod]
@@ -31,16 +37,79 @@ namespace UnitTest_Simplex
             int row = 2;
             int column = 2;
             int[] listCP = { 0, 1 };
-            int[,] listContrainte = { { 1, 2 }, { 2, 3 } };
+            double[,] listContrainte = { { 1, 2 }, { 2, 3 } };
 
             //Appel methode
-            List<double> valueZj = Back.calculZj(listCP, listContrainte, row, column);
+            double[] valueZj = Back.calculZj(listCP, listContrainte, row, column);
 
             //Resultat
-            List<double> testValue = new List<double> { 0, 5 };
+            double[] testValue = { 0, 5 };
+
+            //Affichage valeur
+            LOGGER.Info("Valeur de Zj : " + valueZj);
 
             //Test
-            Assert.AreEqual(valueZj == testValue, "Ok");
+            Assert.AreEqual(valueZj[0] , testValue[0]);
+            Assert.AreEqual(valueZj[1] , testValue[1]);
+        }
+
+        [TestMethod]
+        public void TestSoustractionCjZj()
+        {
+            //Preparation
+            int[] listCj = { 100, 120 };
+            double[] listZj = { 100, 120 };
+
+            //Appel methode
+            double[] valueCjZj = Back.calculSoustractionCjZj(listCj,listZj);
+
+            //Resultat
+            double[] testValue = { 0, 0 };
+
+            //Affichage valeur
+            LOGGER.Info("Valeur de CjZj : " + valueCjZj);
+
+            //Test
+            Assert.AreEqual(valueCjZj[0] , testValue[0]);
+            Assert.AreEqual(valueCjZj[1] , testValue[1]);
+        }
+
+        [TestMethod]
+        public void TestMinCjZj()
+        {
+            //Preparation
+            double[] listCjZj = { 100, 120, -20 };
+
+            //Appel methode
+            int result = Back.minCjZJ(listCjZj);
+
+            //Resultat
+            int testValuePlace = 2;
+
+            //Affichage valeur
+            LOGGER.Info("Min de CjZj : " + result);
+
+            //Test
+            Assert.AreEqual(result, testValuePlace);
+        }
+
+        [TestMethod]
+        public void TestMaxCjZj()
+        {
+            //Preparation
+            double[] listCjZj = { 100, 120, -20 };
+
+            //Appel methode
+            int result = Back.maxCjZJ(listCjZj);
+
+            //Resultat
+            int testValuePlace = 1;
+
+            //Affichage valeur
+            LOGGER.Info("Min de CjZj : " + result);
+
+            //Test
+            Assert.AreEqual(result, testValuePlace);
         }
     }
 }
--- algoSimplex/algoSimplex/Back.cs
+++ algoSimplex/algoSimplex/Back.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
 
 namespace algoSimplex
 {
     public static class Back
     {
+        private static ILog LOGGER = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         public static void maximiser()
         {
             Console.Out.WriteLine("Maximiser");
+        }
+
+        public static void minimiser()
+        {
+            Console.Out.WriteLine("Minimiser");
         }
 
         public static double calculZ(int[] listCP, int[] listQuantite)
@@ -22,27 +30,84 @@ namespace algoSimplex
                 {
                         valeurZ += Convert.ToDouble(listCP[i]) * Convert.ToDouble(listQuantite[i]);
                 }
+                LOGGER.Info("Valeur de Z : " + valeurZ);
                 return valeurZ;
             }
             return 0;
         }
 
-        public static List<double> calculZj(int[] listCP, int[,] listContrainte, int pRow, int pColumn)
+        public static double[] calculZj(int[] listCP, double[,] listContrainte, int pRow, int pColumn)
         {
-            List<double> valeurZj = new List<double>();
+            double[] valeurZj = new double[pColumn];
 
-            for(int row = 0; row < pRow; row++)
+            for (int row = 0; row < pRow; row++)
             {
                 double value = 0;
                 for (int column = 0; column < pColumn; column++)
                 {
+                    LOGGER.Info("Valeur boucle :" + value);
                     value += listCP[row] * listContrainte[row, column];
                 }
-                valeurZj.Add(value);
+                LOGGER.Info("Value :" + value);
+                valeurZj[row] = value;
+                LOGGER.Info("ValeurZj :" + valeurZj[row]);
             }
 
             return valeurZj;
         }
+
+        public static double[] calculSoustractionCjZj(int[] listCj, double[] listZj)
+        {
+            double[] result = new double[listCj.Length];
+
+            for(int i = 0; i < listCj.Length; i++)
+            {
+                result[i] = listCj[i] - listZj[i];
+            }
+
+            return result;
+        }
+
+        public static int minCjZJ(double[] listCjZj)
+        {
+            double value = Int32.MaxValue;
+            int result = 0;
+            for(int i = 0; i < listCjZj.Length; i++)
+            {
+                value = Math.Min(value, listCjZj[i]);
+            }
+
+            for (int i = 0; i < listCjZj.Length; i++)
+            {
+                if (value.Equals(listCjZj[i]))
+                {
+                    result = i;
+                }
+            }
+
+            return result;
+        }
+
+        public static int maxCjZJ(double[] listCjZj)
+        {
+            double value = Int32.MinValue;
+            int result = 0;
+            for (int i = 0; i < listCjZj.Length; i++)
+            {
+                value = Math.Max(value, listCjZj[i]);
+            }
+
+            for (int i = 0; i < listCjZj.Length; i++)
+            {
+                if (value.Equals(listCjZj[i]))
+                {
+                    result = i;
+                }
+            }
+
+            return result;
+        }
+
 
     }
 }
